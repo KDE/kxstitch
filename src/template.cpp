502a352b20fcacf2bd83749da8b99d7e96f7a9c4
Stephen Allewell <stephen@mirramar.adsl24.co.uk>
2011-06-30 13:39:25 +0100

    Removed old source files.

--- src/template.cpp
+++
@@ -1,9 +0,0 @@
-/****************************************************************************
- *	Copyright (C) 2010 by Stephen Allewell									*
- *	stephen@mirramar.adsl24.co.uk											*
- *																			*
- *	This program is free software; you can redistribute it and/or modify	*
- *	it under the terms of the GNU General Public License as published by	*
- *	the Free Software Foundation; either version 2 of the License, or		*
- *	(at your option) any later version.										*
- ****************************************************************************/
