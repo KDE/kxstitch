3a097081c89d67785d7162846195949f33800bb6
Stephen Allewell <stephen@mirramar.adsl24.co.uk>
2012-05-15 23:50:25 +0100

    Change build scripts to use sudo instead of su

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 rm -rf build
 mkdir build
 if [ -d "build" ]; then
-	cd build
-	THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
-	cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. && make -j${THREADS} && su -c 'make install'
+    cd build
+    THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+    cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. && make -j${THREADS} && sudo make install
 else
-	echo "Unable to create build directory. Build aborted\n"
+    echo "Unable to create build directory. Build aborted\n"
 fi
 
