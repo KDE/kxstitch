KDE/kxstitch

commit a3286299777def8b72932f1851890559b126cf05

Changed import image to a QUndoCommand based command
Importing an image didn't mark the file as changed because changes are
identified through the undo/redo stack not being empty.  Now being based
on QUndoCommand shows the file as dirty when an image is imported and
also allows it to be undone/redone.

src/MainWindow.cpp
@@ -717,12 +717,12 @@ void MainWindow::convertImage(const Magick::Image &image)
             documentHeight /= 2;
         }
 
-        m_document->pattern()->stitches().resize(documentWidth, documentHeight);
-
         QString schemeName = importImageDlg->flossScheme();
-        
         FlossScheme *flossScheme = SchemeManager::scheme(schemeName);
-        m_document->pattern()->palette().setSchemeName(schemeName);
+
+        QUndoCommand *importImageCommand = new ImportImageCommand(m_document);        
+        new ResizeDocumentCommand(m_document, documentWidth, documentHeight, importImageCommand);
+        new ChangeSchemeCommand(m_document, schemeName, importImageCommand);
 
         QProgressDialog progress(i18n("Converting to stitches"), i18n("Cancel"), 0, pixelCount, this);
         progress.setWindowModality(Qt::WindowModal);
@@ -735,7 +735,9 @@ void MainWindow::convertImage(const Magick::Image &image)
             QApplication::processEvents();
 
             if (progress.wasCanceled()) {
-                break;
+                delete importImageDlg;
+                delete importImageCommand;
+                return;
             }
 
             for (int dx = 0 ; dx < imageWidth ; dx++) {
@@ -763,7 +765,7 @@ void MainWindow::convertImage(const Magick::Image &image)
                             
                             DocumentFloss *documentFloss = new DocumentFloss(foundName, stitchSymbol, backstitchSymbol, Configuration::palette_StitchStrands(), Configuration::palette_BackstitchStrands());
                             documentFloss->setFlossColor(color);
-                            m_document->pattern()->palette().add(flossIndex, documentFloss);
+                            new AddDocumentFlossCommand(m_document, flossIndex, documentFloss, importImageCommand);
                             documentFlosses.insert(flossIndex, color);
                             flossSymbols.insert(flossIndex, stitchSymbol);
                         }
@@ -772,9 +774,9 @@ void MainWindow::convertImage(const Magick::Image &image)
                         //   flossIndex will be the index for the found color
                         if (useFractionals) {
                             int zone = (dy % 2) * 2 + (dx % 2);
-                            m_document->pattern()->stitches().addStitch(QPoint(dx / 2, dy / 2), stitchMap[0][zone], flossIndex);
+                            new AddStitchCommand(m_document, QPoint(dx / 2, dy / 2), stitchMap[0][zone], flossIndex, importImageCommand);
                         } else {
-                            m_document->pattern()->stitches().addStitch(QPoint(dx, dy), Stitch::Full, flossIndex);
+                            new AddStitchCommand(m_document, QPoint(dx, dy), Stitch::Full, flossIndex, importImageCommand);
                         }
                     }
                 }
@@ -788,22 +790,17 @@ void MainWindow::convertImage(const Magick::Image &image)
             kDebug() << "Found a missing color name and attempting to fix";
             
             if (schemeName == "DMC") {
-                m_document->pattern()->palette().setSchemeName("Anchor");
+                new ChangeSchemeCommand(m_document, "Anchor", importImageCommand);
             } else {
-                m_document->pattern()->palette().setSchemeName("DMC");
+                new ChangeSchemeCommand(m_document, "DMC", importImageCommand);
             }
-            
-            m_document->pattern()->palette().setSchemeName(schemeName);
+  
+            new ChangeSchemeCommand(m_document, schemeName, importImageCommand);
         }              
 
-        m_document->setProperty("horizontalClothCount", importImageDlg->horizontalClothCount());
-        m_document->setProperty("verticalClothCount", importImageDlg->verticalClothCount());
-        m_editor->readDocumentSettings();
-        m_palette->readDocumentSettings();
-        m_preview->readDocumentSettings();
-        m_editor->update();
-        m_palette->update();
-        m_preview->update();
+        new SetPropertyCommand(m_document, "horizontalClothCount", importImageDlg->horizontalClothCount(), importImageCommand);
+        new SetPropertyCommand(m_document, "verticalClothCount", importImageDlg->verticalClothCount(), importImageCommand);
+        m_document->undoStack().push(importImageCommand);
     }
 
     delete importImageDlg;

src/Commands.cpp
@@ -64,6 +64,42 @@ void FilePropertiesCommand::undo()
 }
 
 
+ImportImageCommand::ImportImageCommand(Document *document)
+    :   QUndoCommand(i18n("Import Image")),
+        m_document(document)
+{
+}
+                      
+                      
+ImportImageCommand::~ImportImageCommand()
+{
+}
+                      
+                      
+void ImportImageCommand::redo()
+{
+    QUndoCommand::redo();
+    m_document->editor()->readDocumentSettings();
+    m_document->preview()->readDocumentSettings();
+    m_document->palette()->readDocumentSettings();
+    m_document->editor()->update();
+    m_document->preview()->update();
+    m_document->palette()->update();
+}
+                      
+                      
+void ImportImageCommand::undo()
+{
+    QUndoCommand::undo();
+    m_document->editor()->readDocumentSettings();
+    m_document->preview()->readDocumentSettings();
+    m_document->palette()->readDocumentSettings();
+    m_document->editor()->update();
+    m_document->preview()->update();
+    m_document->palette()->update();
+}
+                      
+                      
 PaintStitchesCommand::PaintStitchesCommand(Document *document)
     :   QUndoCommand(i18n("Paint Stitches")),
         m_document(document)
@@ -662,8 +698,8 @@ void RemoveBackgroundImageCommand::undo()
 }
 
 
-AddDocumentFlossCommand::AddDocumentFlossCommand(Document *document, int key, DocumentFloss *documentFloss)
-    :   QUndoCommand(),
+AddDocumentFlossCommand::AddDocumentFlossCommand(Document *document, int key, DocumentFloss *documentFloss, QUndoCommand *parent)
+    :   QUndoCommand(parent),
         m_document(document),
         m_key(key),
         m_documentFloss(documentFloss)

src/Commands.h
@@ -52,6 +52,20 @@ private:
 };
 
 
+class ImportImageCommand : public QUndoCommand
+{
+public:
+    explicit ImportImageCommand(Document *);
+    virtual ~ImportImageCommand();
+
+    virtual void redo();
+    virtual void undo();
+    
+private:
+    Document    *m_document;
+};
+
+
 class PaintStitchesCommand : public QUndoCommand
 {
 public:
@@ -366,7 +380,7 @@ private:
 class AddDocumentFlossCommand : public QUndoCommand
 {
 public:
-    AddDocumentFlossCommand(Document *, int, DocumentFloss *);
+    AddDocumentFlossCommand(Document *, int, DocumentFloss *, QUndoCommand *parent = 0);
     ~AddDocumentFlossCommand();
 
     virtual void redo();

commit f30950c79763b61de6bccd441a8b9ee21116f6b1

Bug fix for importing images using the same symbol
Commit a3286299777def8b72932f1851890559b126cf05 caused a bug that used
the same symbol for every color due to the delayed insertion of the
flosses into the document reserving symbols.  Symbols are now reserved
within the convert image function instead of from the documents existing
palette.

src/MainWindow.cpp
@@ -60,6 +60,8 @@
 #include "QVariantPtr.h"
 #include "Scale.h"
 #include "SchemeManager.h"
+#include "SymbolLibrary.h"
+#include "SymbolManager.h"
 
 
 MainWindow::MainWindow()
@@ -693,7 +695,7 @@ void MainWindow::fileImportImage()
 void MainWindow::convertImage(const Magick::Image &image)
 {
     QMap<int, QColor> documentFlosses;
-    QMap<int, qint16> flossSymbols;
+    QList<qint16> symbolIndexes = SymbolManager::library("kxstitch")->indexes();
 
     QPointer<ImportImageDlg> importImageDlg = new ImportImageDlg(this, image);
 
@@ -755,7 +757,7 @@ void MainWindow::convertImage(const Magick::Image &image)
                         }
 
                         if (flossIndex == documentFlosses.count()) { // reached the end of the list
-                            qint16 stitchSymbol = m_document->pattern()->palette().freeSymbol();
+                            qint16 stitchSymbol = symbolIndexes.takeFirst();
                             Qt::PenStyle backstitchSymbol(Qt::SolidLine);
                             QString foundName = flossScheme->find(color);
 
@@ -767,7 +769,6 @@ void MainWindow::convertImage(const Magick::Image &image)
                             documentFloss->setFlossColor(color);
                             new AddDocumentFlossCommand(m_document, flossIndex, documentFloss, importImageCommand);
                             documentFlosses.insert(flossIndex, color);
-                            flossSymbols.insert(flossIndex, stitchSymbol);
                         }
 
                         // at this point