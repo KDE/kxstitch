1d07e185fc9605e426ab050a5e98cfb3cd3f61cb
Steve Allewell <steve.allewell@gmail.com>
2015-09-02 21:10:44 +0100

    Fix to prevent the same symbol being applied to two flosses
    Despite setting the used symbols as not enabled, the QListWidget still
    emits the itemClicked signal when the mouse is clicked on a disabled
    item. This fix test the state and ignores disabled items.
    
    (cherry picked from commit 480aa9b16f1207581bc9d3dad174e4c0a710b856)

--- src/SymbolSelectorDlg.cpp
+++ src/SymbolSelectorDlg.cpp
@@ -95,15 +95,16 @@ qint16 SymbolSelectorDlg::selectedSymbol()
  *
  * @param item a pointer to the QListWidgetItem that was selected
  */
-void SymbolSelectorDlg::on_SymbolTable_executed(QListWidgetItem *item)
+void SymbolSelectorDlg::on_SymbolTable_itemClicked(QListWidgetItem *item)
 {
-    m_usedSymbols.removeOne(m_currentSymbol);
-    ui.SymbolTable->enableItem(m_symbolItems.value(m_currentSymbol));
-    m_currentSymbol = static_cast<qint16>(item->data(Qt::UserRole).toInt());
-    ui.SymbolTable->disableItem(m_symbolItems.value(m_currentSymbol));
-    m_usedSymbols.append(m_currentSymbol);
-
-    accept();
+    if (item->flags() & Qt::ItemIsEnabled) {
+        m_usedSymbols.removeOne(m_currentSymbol);
+        ui.SymbolTable->enableItem(m_symbolItems.value(m_currentSymbol));
+        m_currentSymbol = static_cast<qint16>(item->data(Qt::UserRole).toInt());
+        ui.SymbolTable->disableItem(m_symbolItems.value(m_currentSymbol));
+        m_usedSymbols.append(m_currentSymbol);
+        accept();
+    }
 }
 
 
