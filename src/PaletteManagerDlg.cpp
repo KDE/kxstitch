61afd22ecc84c3af4c68a926a45c22c05bb7b511
Steve Allewell <steve.allewell@gmail.com>
2015-09-13 00:11:01 +0100

    Show the name of the current scheme in the palette manager dialog
    (cherry picked from commit 5d771309ab051cf11fd3641e213ef8452c5ecff0)
    
    Conflicts:
    	ui/PaletteManager.ui

--- src/PaletteManagerDlg.cpp
+++ src/PaletteManagerDlg.cpp
@@ -290,6 +290,7 @@ void PaletteManagerDlg::fillLists()
 {
     ui.ColorList->clear();
     ui.CurrentList->clear();
+    ui.CurrentScheme->setText(m_scheme->schemeName());
 
     foreach (const Floss * floss, m_scheme->flosses()) {
         QListWidgetItem *listWidgetItem = new QListWidgetItem;
