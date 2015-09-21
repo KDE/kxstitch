KDE/kxstitch

commit 8d85703686c20dccb4f118afef585fc989ffc1ab

Create dialogs ui on QDialog rather that child QWidget
This fixes the lack of resizing of dialog contents when the dialog is
resized.  This bug was introduced when changing KDialog to QDialog as
the QWidget is no longer set as the main widget of the dialog.

src/CalibrateFlossDlg.cpp
@@ -26,8 +26,7 @@ CalibrateFlossDlg::CalibrateFlossDlg(QWidget *parent, const QString &schemeName)
         m_sample(0)
 {
     setWindowTitle(i18n("Calibrate Floss"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
     QMetaObject::connectSlotsByName(this);
 
     fillSchemeList();

src/ExtendPatternDlg.cpp
@@ -20,8 +20,7 @@ ExtendPatternDlg::ExtendPatternDlg(QWidget *parent)
     :   QDialog(parent)
 {
     setWindowTitle(i18n("Extend Pattern"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
     QMetaObject::connectSlotsByName(this);
 }
 

src/FilePropertiesDlg.cpp
@@ -28,8 +28,7 @@ FilePropertiesDlg::FilePropertiesDlg(QWidget *parent, Document *document)
         m_document(document)
 {
     setWindowTitle(i18n("File Properties"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
 
     QRect extents = m_document->pattern()->stitches().extents();
     m_minWidth = extents.width();

src/ImageElementDlg.cpp
@@ -27,8 +27,7 @@ ImageElementDlg::ImageElementDlg(QWidget *parent, ImageElement *imageElement, Do
         m_document(document)
 {
     setWindowTitle(i18n("Image Element Properties"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
 
     m_selectArea = new SelectArea(ui.PreviewFrame, imageElement, document, QMap<int, QList<QRect> >());
     m_selectArea->setPatternRect(imageElement->patternRect());

src/ImportImageDlg.cpp
@@ -30,8 +30,7 @@ ImportImageDlg::ImportImageDlg(QWidget *parent, const Magick::Image &originalIma
         m_alphaSelect(0),
         m_originalImage(originalImage)
 {
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
 
     ui.FlossScheme->addItems(SchemeManager::schemes());
 

src/KeyElementDlg.cpp
@@ -24,8 +24,7 @@ KeyElementDlg::KeyElementDlg(QWidget *parent, KeyElement *keyElement)
         m_keyElement(keyElement)
 {
     setWindowTitle(i18n("Key Element Properties"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
 
     ui.MarginLeft->setValue(keyElement->m_margins.left());
     ui.MarginTop->setValue(keyElement->m_margins.top());

src/LibraryFilePathsDlg.cpp
@@ -19,8 +19,7 @@ LibraryFilePathsDlg::LibraryFilePathsDlg(QWidget *parent, const QString&, QStrin
     :   QDialog(parent)
 {
     setWindowTitle(i18n("Library File Paths"));
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
     QMetaObject::connectSlotsByName(this);
 
     QStringListIterator pathIterator(paths);

src/LibraryManagerDlg.cpp
@@ -38,8 +38,7 @@ LibraryManagerDlg::LibraryManagerDlg(QWidget *parent)
 {
     setWindowTitle(i18n("Library Manager"));
 
-    QWidget *widget = new QWidget(this);
-    ui.setupUi(widget);
+    ui.setupUi(this);
     ui.ZoomOut->setIcon(QIcon::fromTheme("zoom-out"));
     ui.ZoomIn->setIcon(QIcon::fromTheme("zoom-in"));
     QMetaObject::connectSlotsByName(this);

commit 0a312b59fb125cb68c01c4d0a78f7bf8187f7af9

Fixed double connection of dialog signals and slots
This bug was introduced in commit 8d85703686c20dccb4f118afef585fc989ffc1ab
Using setupUi directly on the QDialog automatically calls connectSlotsByName
so calling in the constructor causes a double connection and double calls
to the slots.

src/CalibrateFlossDlg.cpp
@@ -27,7 +27,6 @@ CalibrateFlossDlg::CalibrateFlossDlg(QWidget *parent, const QString &schemeName)
 {
     setWindowTitle(i18n("Calibrate Floss"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     fillSchemeList();
     ui.SchemeList->setCurrentIndex(ui.SchemeList->findText(schemeName));

src/ConfigurationDialogs.cpp
@@ -32,8 +32,6 @@ PatternConfigPage::PatternConfigPage(QWidget *parent, const char *name)
     setObjectName(name);
     setupUi(this);
 
-    QMetaObject::connectSlotsByName(this);
-
     m_currentDocumentUnitsIndex = Configuration::document_UnitsFormat();
     m_currentClothCountUnitsIndex = Configuration::editor_ClothCountUnits();
     kcfg_Editor_VerticalClothCount->setEnabled(!Configuration::editor_ClothCountLink());

src/ExtendPatternDlg.cpp
@@ -21,7 +21,6 @@ ExtendPatternDlg::ExtendPatternDlg(QWidget *parent)
 {
     setWindowTitle(i18n("Extend Pattern"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 }
 
 

src/FilePropertiesDlg.cpp
@@ -52,8 +52,6 @@ FilePropertiesDlg::FilePropertiesDlg(QWidget *parent, Document *document)
     ui.Instructions->setPlainText(m_document->property("instructions").toString());
     ui.ClothCountLink->setChecked(m_clothCountLink);
     on_ClothCountLink_clicked(m_clothCountLink);
-
-    QMetaObject::connectSlotsByName(this);
 }
 
 

src/ImageElementDlg.cpp
@@ -36,8 +36,6 @@ ImageElementDlg::ImageElementDlg(QWidget *parent, ImageElement *imageElement, Do
     ui.ShowBorder->setChecked(imageElement->showBorder());
     ui.BorderColor->setColor(imageElement->borderColor());
     ui.BorderThickness->setValue(double(imageElement->borderThickness()) / 10);
-
-    QMetaObject::connectSlotsByName(this);
 }
 
 

src/ImportImageDlg.cpp
@@ -43,8 +43,6 @@ ImportImageDlg::ImportImageDlg(QWidget *parent, const Magick::Image &originalIma
     m_useFractionals = ui.UseFractionals->isChecked();
     m_ignoreColor = ui.IgnoreColor->isChecked();
 
-    QMetaObject::connectSlotsByName(this);
-
     createImageMap();
     renderPixmap();
 }

src/KeyElementDlg.cpp
@@ -46,8 +46,6 @@ KeyElementDlg::KeyElementDlg(QWidget *parent, KeyElement *keyElement)
     ui.StitchesColumn->setChecked(m_keyElement->m_stitchesColumn);
     ui.LengthColumn->setChecked(m_keyElement->m_lengthColumn);
     ui.SkeinsColumn->setChecked(m_keyElement->m_skeinsColumn);
-
-    QMetaObject::connectSlotsByName(this);
 }
 
 

src/LibraryFilePathsDlg.cpp
@@ -20,7 +20,6 @@ LibraryFilePathsDlg::LibraryFilePathsDlg(QWidget *parent, const QString&, QStrin
 {
     setWindowTitle(i18n("Library File Paths"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     QStringListIterator pathIterator(paths);
 

src/LibraryManagerDlg.cpp
@@ -41,7 +41,6 @@ LibraryManagerDlg::LibraryManagerDlg(QWidget *parent)
     ui.setupUi(this);
     ui.ZoomOut->setIcon(QIcon::fromTheme("zoom-out"));
     ui.ZoomIn->setIcon(QIcon::fromTheme("zoom-in"));
-    QMetaObject::connectSlotsByName(this);
 
     refreshLibraries();
 

src/LibraryPatternPropertiesDlg.cpp
@@ -20,7 +20,7 @@ LibraryPatternPropertiesDlg::LibraryPatternPropertiesDlg(QWidget *parent, qint32
 {
     setWindowTitle(i18n("Library Pattern Properties"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
+
     ui.FlossScheme->setText(scheme);
     ui.Width->setText(QString().setNum(width));
     ui.Height->setText(QString().setNum(height));

src/NewFlossDlg.cpp
@@ -25,7 +25,7 @@ NewFlossDlg::NewFlossDlg(QWidget *parent, FlossScheme *flossScheme)
 {
     setWindowTitle(i18n("New Floss"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
+
     ui.SchemeName->setText(m_flossScheme->schemeName());
     ui.DialogButtonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 }

src/PagePropertiesDlg.cpp
@@ -24,7 +24,6 @@ PagePropertiesDlg::PagePropertiesDlg(QWidget *parent, const QMargins &margins, b
 {
     setWindowTitle(i18n("Page Properties"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     ui.MarginTop->setValue(margins.top());
     ui.MarginLeft->setValue(margins.left());

src/PaletteManagerDlg.cpp
@@ -41,7 +41,6 @@ PaletteManagerDlg::PaletteManagerDlg(QWidget *parent, Document *document)
 {
     setWindowTitle(i18n("Palette Manager"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     ui.SymbolLibrary->insertItems(0, SymbolManager::libraries());
     ui.SymbolLibrary->setCurrentItem(m_dialogPalette.symbolLibrary());

src/PatternElementDlg.cpp
@@ -60,8 +60,6 @@ PatternElementDlg::PatternElementDlg(QWidget *parent, PatternElement *patternEle
     ui.RenderStitchesAs->setCurrentIndex(ui.RenderStitchesAs->findData(patternElement->m_renderStitchesAs));
     ui.RenderBackstitchesAs->setCurrentIndex(ui.RenderBackstitchesAs->findData(patternElement->m_renderBackstitchesAs));
     ui.RenderKnotsAs->setCurrentIndex(ui.RenderKnotsAs->findData(patternElement->m_renderKnotsAs));
-
-    QMetaObject::connectSlotsByName(this);
 }
 
 

src/PrintSetupDlg.cpp
@@ -70,7 +70,6 @@ PrintSetupDlg::PrintSetupDlg(QWidget *parent, Document *document, QPrinter *prin
     m_buttonGroup.addButton(ui.KeyElement);
     m_buttonGroup.setExclusive(true);
 
-    QMetaObject::connectSlotsByName(this);
     connect(m_pageLayoutEditor, SIGNAL(selectionMade(QRect)), this, SLOT(selectionMade(QRect)));
     connect(m_pageLayoutEditor, SIGNAL(elementGeometryChanged()), this, SLOT(elementGeometryChanged()));
     connect(m_pageLayoutEditor, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(previewContextMenuRequested(QPoint)));

src/SymbolSelectorDlg.cpp
@@ -49,7 +49,6 @@ SymbolSelectorDlg::SymbolSelectorDlg(QWidget *parent, const QString &symbolLibra
 {
     setWindowTitle(i18n("Symbol Selector"));
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     SymbolLibrary *library = SymbolManager::library(symbolLibrary);
 

src/TextElementDlg.cpp
@@ -30,7 +30,6 @@ TextElementDlg::TextElementDlg(QWidget *parent, TextElement *textElement)
     setWindowTitle(i18n("Text Element Properties"));
 
     ui.setupUi(this);
-    QMetaObject::connectSlotsByName(this);
 
     ui.BoldButton->setIcon(QIcon::fromTheme("format-text-bold"));
     ui.UnderlineButton->setIcon(QIcon::fromTheme("format-text-underline"));

src/TextToolDlg.cpp
@@ -30,8 +30,6 @@ TextToolDlg::TextToolDlg(QWidget *parent)
 
     ui.TextToolFont->setCurrentFont(QFont(Configuration::textTool_FontFamily()));
     ui.TextToolSize->setValue(Configuration::textTool_FontSize());
-
-    QMetaObject::connectSlotsByName(this);
 }
 
 