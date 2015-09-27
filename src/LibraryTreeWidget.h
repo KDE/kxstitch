beab2eefff28b0de3bfa1cf3c4f1d53495241e5c
Steve Allewell <steve.allewell@gmail.com>
2015-09-27 11:32:12 +0100

    Workaround for auto expand QTreeWigetItems not working
    Implemented a QTimer to fire when hovering over a QTreeWidgetItem which
    then expands the item.
    
    Fixed missing setAcceptDrops() which I inadvertantly deleted during clean
    up of the properties in the ui file.

--- src/LibraryTreeWidget.h
+++ src/LibraryTreeWidget.h
@@ -20,6 +20,7 @@ class QDragEnterEvent;
 class QDragLeaveEvent;
 class QDragMoveEvent;
 class QDropEvent;
+class QTimer;
 class QTreeWidgetItem;
 class QWidget;
 
@@ -30,6 +31,7 @@ class LibraryTreeWidget : public QTreeWidget
 
 public:
     explicit LibraryTreeWidget(QWidget *parent);
+    virtual ~LibraryTreeWidget();
 
 protected:
     virtual void dragEnterEvent(QDragEnterEvent *event);
@@ -38,7 +40,11 @@ protected:
     virtual void dropEvent(QDropEvent *event);
     virtual bool acceptDrag(QDropEvent *event) const;
 
+private slots:
+    void openBranch();
+
 private:
+    QTimer          *m_openBranchTimer;
     QTreeWidgetItem *m_dropItem;
     QTreeWidgetItem *m_currentItem;
 };
